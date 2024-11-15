c6acc3bf6ed91803a5e834688ea4ed84e9b1bb59
kader009 <kadermollarasel000@gmail.com>
2024-11-15 10:20:46 +0600

    user data create for show

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
+import UserData from '@/components/user/page';
+
 const Home = () => {
   console.log('change');
-  return <div className="text-blue-500 font-semibold text-center">Home page</div>;
+  return (
+    <div className="text-blue-500 font-semibold flex justify-center items-center min-h-screen">
+      <UserData />
+    </div>
+  );
 };
 
 export default Home;
