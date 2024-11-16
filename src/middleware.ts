c4ecea1df732f4a5001f7eda4095038be022ecb8
kader009 <kadermollarasel000@gmail.com>
2024-11-16 11:11:33 +0600

    middleware add

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,10 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export function middleware(request: NextRequest) {
+  console.log('middleware');
   return NextResponse.redirect(new URL('/', request.url));
 }
 
 export const config = {
-  matcher: ['/about'],
+  matcher: ['/layout/:path*'],
 };
