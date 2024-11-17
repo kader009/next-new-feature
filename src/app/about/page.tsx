b99f3324167ebbdd18580dc739702d9c989ab0fd
kader009 <kadermollarasel000@gmail.com>
2024-11-17 10:52:43 +0600

    create mock api

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,19 @@
 import { use } from 'react';
 
+const fetchName = async () => {
+  const response = await fetch(
+    `https://6736d36baafa2ef22231a2ab.mockapi.io/name`
+  );
+  const data = response.json();
+  return data;
+};
+
 const About = () => {
   return (
     <div>
-      <h1>About page</h1>
-      
+      <h1 className="text-center font-semibold">About page</h1>
+      <br />
+      <br />
     </div>
   );
 };
