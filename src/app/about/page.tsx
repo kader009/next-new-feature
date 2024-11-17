83e23ded04e60fdb8ec5acfcbdd0631db06b3225
kader009 <kadermollarasel000@gmail.com>
2024-11-17 10:41:15 +0600

    redesign about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,17 +1,10 @@
-import Image from 'next/image';
+import { use } from 'react';
 
 const About = () => {
   return (
     <div>
-      About
-      <Image
-        src={
-          'https://images.pexels.com/photos/28965902/pexels-photo-28965902/free-photo-of-gardener-in-autumn-sunflower-garden.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-        }
-        alt="cowgirl"
-        width={1000}
-        height={60000}
-      />
+      <h1>About page</h1>
+      
     </div>
   );
 };
