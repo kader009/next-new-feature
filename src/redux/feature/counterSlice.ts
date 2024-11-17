5df9e347971797697c46a52ffb113fe1a3c72cdb
kader009 <kadermollarasel000@gmail.com>
2024-11-17 20:36:18 +0600

    counter slice add with counter

--- src/redux/feature/counterSlice.ts
+++ src/redux/feature/counterSlice.ts
@@ -11,5 +11,17 @@ const initialState: Count = {
 const counterSlice = createSlice({
   name: 'counter',
   initialState,
-  reducers: {},
+  reducers: {
+    increment: (state) => {
+      state.count += 1;
+    },
+
+    decrement: (state) => {
+      state.count -= 1;
+    },
+  },
 });
+
+export default counterSlice.reducer;
+
+export const { decrement, increment } = counterSlice.actions;
