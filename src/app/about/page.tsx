d801164d2e309d3c719d5651ae1975664329619e
kader009 <kadermollarasel000@gmail.com>
2024-11-17 11:12:18 +0600

    add name

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,16 +4,26 @@ const fetchName = async () => {
   const response = await fetch(
     `https://6736d36baafa2ef22231a2ab.mockapi.io/name`
   );
-  const data = response.json();
-  return data;
+
+  return response.json();
 };
 
 const About = () => {
+  const Names = use(fetchName());
   return (
     <div>
       <h1 className="text-center font-semibold">About page</h1>
       <br />
       <br />
+      <div className="flex justify-center items-center gap-4 flex-wrap">
+        {Names.map((name) => (
+          <div key={name.id}>
+            <span className="bg-blue-600 rounded text-white p-2">
+              {name.name}
+            </span>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
