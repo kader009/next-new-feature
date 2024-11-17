4c393e523e8f795967faf1e33c891636e646c6e6
kader009 <kadermollarasel000@gmail.com>
2024-11-17 18:57:30 +0600

    hooks add

--- src/provider/provider.tsx
+++ src/provider/provider.tsx
@@ -1,9 +1,11 @@
+'use client';
+
 import { store } from '@/redux/store';
 import React, { ReactNode } from 'react';
 import { Provider } from 'react-redux';
 
-const provider = ({ children }: { children: ReactNode }) => {
+const Providers = ({ children }: { children: ReactNode }) => {
   return <Provider store={store}>{children}</Provider>;
 };
 
-export default provider;
+export default Providers;
