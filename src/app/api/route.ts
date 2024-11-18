52868158a8ba3386dca1567cf0667237f7f10146
kader009 <kadermollarasel000@gmail.com>
2024-11-18 11:59:57 +0600

    cache get data

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,3 +1,7 @@
 export async function GET(){
-  return new Response(JSON.stringify({message:'Hello world'}))
+  return new Response(JSON.stringify({message:'Hello world'}),{
+    headers:{
+      'Cache-Control':'s-maxage=60, stale-while-revalidate=30'
+    }
+  })
 }
